c1531dc88c63a4197f88af9d6387255d1b1c9c2d
Milos Tomasevic <milosnstomasevic@gmail.com>
2020-08-24 02:22:43 +0200

    Napravljen pocetni meni i zapoceta stranica vozaca

---
+++ scripts/script.js
@@ -0,0 +1,319 @@
+var array = [{
+    "id": 1,
+    "name": "Ivan",
+    "lastName": "Kocoljevac",
+    "umcn": "154785968459",
+    "workbookNumber": "254569"
+}, {
+    "id": 2,
+    "name": "Milos",
+    "lastName": "Tomasevic",
+    "umcn": "521236548512",
+    "workbookNumber": "856987"
+}, {
+    "id": 1,
+    "name": "Ivan",
+    "lastName": "Kocoljevac",
+    "umcn": "154785968459",
+    "workbookNumber": "254569"
+}, {
+    "id": 1,
+    "name": "Ivan",
+    "lastName": "Kocoljevac",
+    "umcn": "154785968459",
+    "workbookNumber": "254569"
+}, {
+    "id": 1,
+    "name": "Ivan",
+    "lastName": "Kocoljevac",
+    "umcn": "154785968459",
+    "workbookNumber": "254569"
+}, {
+    "id": 2,
+    "name": "Milos",
+    "lastName": "Tomasevic",
+    "umcn": "521236548512",
+    "workbookNumber": "856987"
+}, {
+    "id": 1,
+    "name": "Ivan",
+    "lastName": "Kocoljevac",
+    "umcn": "154785968459",
+    "workbookNumber": "254569"
+}, {
+    "id": 1,
+    "name": "Ivan",
+    "lastName": "Kocoljevac",
+    "umcn": "154785968459",
+    "workbookNumber": "254569"
+}, {
+    "id": 2,
+    "name": "Milos",
+    "lastName": "Tomasevic",
+    "umcn": "521236548512",
+    "workbookNumber": "856987"
+}, {
+    "id": 1,
+    "name": "Ivan",
+    "lastName": "Kocoljevac",
+    "umcn": "154785968459",
+    "workbookNumber": "254569"
+}, {
+    "id": 1,
+    "name": "Ivan",
+    "lastName": "Kocoljevac",
+    "umcn": "154785968459",
+    "workbookNumber": "254569"
+}, {
+    "id": 2,
+    "name": "Milos",
+    "lastName": "Tomasevic",
+    "umcn": "521236548512",
+    "workbookNumber": "856987"
+}, {
+    "id": 1,
+    "name": "Ivan",
+    "lastName": "Kocoljevac",
+    "umcn": "154785968459",
+    "workbookNumber": "254569"
+}, {
+    "id": 1,
+    "name": "Ivan",
+    "lastName": "Kocoljevac",
+    "umcn": "154785968459",
+    "workbookNumber": "254569"
+}, {
+    "id": 2,
+    "name": "Milos",
+    "lastName": "Tomasevic",
+    "umcn": "521236548512",
+    "workbookNumber": "856987"
+}, {
+    "id": 1,
+    "name": "Ivan",
+    "lastName": "Kocoljevac",
+    "umcn": "154785968459",
+    "workbookNumber": "254569"
+}, {
+    "id": 1,
+    "name": "Ivan",
+    "lastName": "Kocoljevac",
+    "umcn": "154785968459",
+    "workbookNumber": "254569"
+}, {
+    "id": 2,
+    "name": "Milos",
+    "lastName": "Tomasevic",
+    "umcn": "521236548512",
+    "workbookNumber": "856987"
+}, {
+    "id": 1,
+    "name": "Ivan",
+    "lastName": "Kocoljevac",
+    "umcn": "154785968459",
+    "workbookNumber": "254569"
+}, {
+    "id": 1,
+    "name": "Ivan",
+    "lastName": "Kocoljevac",
+    "umcn": "154785968459",
+    "workbookNumber": "254569"
+}, {
+    "id": 2,
+    "name": "Milos",
+    "lastName": "Tomasevic",
+    "umcn": "521236548512",
+    "workbookNumber": "856987"
+}, {
+    "id": 1,
+    "name": "Ivan",
+    "lastName": "Kocoljevac",
+    "umcn": "154785968459",
+    "workbookNumber": "254569"
+}, {
+    "id": 1,
+    "name": "Ivan",
+    "lastName": "Kocoljevac",
+    "umcn": "154785968459",
+    "workbookNumber": "254569"
+}, {
+    "id": 2,
+    "name": "Milos",
+    "lastName": "Tomasevic",
+    "umcn": "521236548512",
+    "workbookNumber": "856987"
+}, {
+    "id": 1,
+    "name": "Ivan",
+    "lastName": "Kocoljevac",
+    "umcn": "154785968459",
+    "workbookNumber": "254569"
+}, {
+    "id": 1,
+    "name": "Ivan",
+    "lastName": "Kocoljevac",
+    "umcn": "154785968459",
+    "workbookNumber": "254569"
+}, {
+    "id": 2,
+    "name": "Milos",
+    "lastName": "Tomasevic",
+    "umcn": "521236548512",
+    "workbookNumber": "856987"
+}, {
+    "id": 1,
+    "name": "Ivan",
+    "lastName": "Kocoljevac",
+    "umcn": "154785968459",
+    "workbookNumber": "254569"
+}, {
+    "id": 1,
+    "name": "Ivan",
+    "lastName": "Kocoljevac",
+    "umcn": "154785968459",
+    "workbookNumber": "254569"
+}, {
+    "id": 2,
+    "name": "Milos",
+    "lastName": "Tomasevic",
+    "umcn": "521236548512",
+    "workbookNumber": "856987"
+}, {
+    "id": 1,
+    "name": "Ivan",
+    "lastName": "Kocoljevac",
+    "umcn": "154785968459",
+    "workbookNumber": "254569"
+}, {
+    "id": 1,
+    "name": "Ivan",
+    "lastName": "Kocoljevac",
+    "umcn": "154785968459",
+    "workbookNumber": "254569"
+}, {
+    "id": 2,
+    "name": "Milos",
+    "lastName": "Tomasevic",
+    "umcn": "521236548512",
+    "workbookNumber": "856987"
+}, {
+    "id": 1,
+    "name": "Ivan",
+    "lastName": "Kocoljevac",
+    "umcn": "154785968459",
+    "workbookNumber": "254569"
+}, {
+    "id": 1,
+    "name": "Ivan",
+    "lastName": "Kocoljevac",
+    "umcn": "154785968459",
+    "workbookNumber": "254569"
+}, {
+    "id": 2,
+    "name": "Milos",
+    "lastName": "Tomasevic",
+    "umcn": "521236548512",
+    "workbookNumber": "856987"
+}, {
+    "id": 1,
+    "name": "Ivan",
+    "lastName": "Kocoljevac",
+    "umcn": "154785968459",
+    "workbookNumber": "254569"
+}, {
+    "id": 1,
+    "name": "Ivan",
+    "lastName": "Kocoljevac",
+    "umcn": "154785968459",
+    "workbookNumber": "254569"
+}, {
+    "id": 2,
+    "name": "Milos",
+    "lastName": "Tomasevic",
+    "umcn": "521236548512",
+    "workbookNumber": "856987"
+}, {
+    "id": 1,
+    "name": "Ivan",
+    "lastName": "Kocoljevac",
+    "umcn": "154785968459",
+    "workbookNumber": "254569"
+}, {
+    "id": 1,
+    "name": "Ivan",
+    "lastName": "Kocoljevac",
+    "umcn": "154785968459",
+    "workbookNumber": "254569"
+}, {
+    "id": 2,
+    "name": "Milos",
+    "lastName": "Tomasevic",
+    "umcn": "521236548512",
+    "workbookNumber": "856987"
+}, {
+    "id": 1,
+    "name": "Ivan",
+    "lastName": "Kocoljevac",
+    "umcn": "154785968459",
+    "workbookNumber": "254569"
+}, {
+    "id": 1,
+    "name": "Ivan",
+    "lastName": "Kocoljevac",
+    "umcn": "154785968459",
+    "workbookNumber": "254569"
+}, {
+    "id": 2,
+    "name": "Milos",
+    "lastName": "Tomasevic",
+    "umcn": "521236548512",
+    "workbookNumber": "856987"
+}, {
+    "id": 1,
+    "name": "Ivan",
+    "lastName": "Kocoljevac",
+    "umcn": "154785968459",
+    "workbookNumber": "254569"
+}, {
+    "id": 1,
+    "name": "Ivan",
+    "lastName": "Kocoljevac",
+    "umcn": "154785968459",
+    "workbookNumber": "254569"
+}, {
+    "id": 2,
+    "name": "Milos",
+    "lastName": "Tomasevic",
+    "umcn": "521236548512",
+    "workbookNumber": "856987"
+}, {
+    "id": 1,
+    "name": "Ivan",
+    "lastName": "Kocoljevac",
+    "umcn": "154785968459",
+    "workbookNumber": "254569"
+}, {
+    "id": 1,
+    "name": "Ivan",
+    "lastName": "Kocoljevac",
+    "umcn": "154785968459",
+    "workbookNumber": "254569"
+}];
+
+buildTable(array);
+
+function buildTable(data) {
+    var table = document.getElementById('myTable');
+
+    for (var i = 0; i < data.length; i++) {
+        var row = `<tr>
+            <td>` + data[i].id + `</td>
+            <td>` + data[i].name + `</td>
+            <td>` + data[i].lastName + `</td>
+            <td>` + data[i].umcn + `</td>
+            <td>` + data[i].workbookNumber + `</td>
+            </tr>`;
+
+        table.innerHTML += row;
+    }
+}
