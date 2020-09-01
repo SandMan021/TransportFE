eb7fdf77d418a8a5ee333ae9791e7c4967a3914e
Milos Tomasevic <milosnstomasevic@gmail.com>
2020-09-01 16:08:42 +0200

    Dodat modalni prozor za prikazi detalje, ispisani podaci o vozacu u modalnom prozoru.

--- scripts/script.js
+++ scripts/script.js
@@ -110,14 +110,16 @@ var array = [{
 
 //http://3.21.92.112:8080/TransportPall-0.0.1-SNAPSHOT/transportPal/driver/getAll
 
-axios.get("http://3.21.92.112:8080/TransportPall-0.0.1-SNAPSHOT/transportPal/driver/getAll",  { crossdomain: true })
-.then(res=>{
-    nekiNizKojiJeStigaoSaAPIJA = res.data;
-    console.log("Ovo je stiglo", nekiNizKojiJeStigaoSaAPIJA);
-})
-.catch(err=>{
-    console.log(err.response);
-});
+axios.get("http://3.21.92.112:8080/TransportPall-0.0.1-SNAPSHOT/transportPal/driver/getAll", {
+        crossdomain: true
+    })
+    .then(res => {
+        nekiNizKojiJeStigaoSaAPIJA = res.data;
+        console.log("Ovo je stiglo", nekiNizKojiJeStigaoSaAPIJA);
+    })
+    .catch(err => {
+        console.log(err.response);
+    });
 
 function addListenersToDelete() {
     let listOfDeleteButtonsInTable = document.getElementsByClassName("btnDelete");
@@ -126,14 +128,30 @@ function addListenersToDelete() {
     }
 }
 
+function addListenersToMoreInfo() {
+    let listOfMoreInfoButtonsInTable = document.getElementsByClassName("btnMoreInfo");
+    for (let i = 0; i < listOfMoreInfoButtonsInTable.length; i++) {
+        listOfMoreInfoButtonsInTable[i].addEventListener("click", showMoreDriverInfo);
+    }
+}
+/*-----------Modal delete------------ */
 document.getElementById("zatvoriModalniDetaljnije").addEventListener("click", openCloseModalWindowDelete);
 document.getElementById("modalDelete").addEventListener("click", checkIfClickedModalDelete);
 document.getElementById("btnCancelDeleteDriver").addEventListener("click", openCloseModalWindowDelete);
 
+/*-----------Modal more info---------------*/
+document.getElementById("closeModalMoreInfo").addEventListener("click", openCloseModalWindowInfo);
+document.getElementById("modalMoreInfo").addEventListener("click", checkIfClickedModalMoreInfo);
 
 function checkIfClickedModalDelete(e) {
     if (e.target === document.getElementById("modalDelete")) {
         openCloseModalWindowDelete();
+    }
+}
+
+function checkIfClickedModalMoreInfo(e) {
+    if (e.target === document.getElementById("modalMoreInfo")) {
+        openCloseModalWindowInfo();
     }
 }
 
@@ -150,22 +168,82 @@ function buildTable(data) {
             <td>` + data[i].umcn + `</td>
             <td>` + data[i].workbookNumber + `</td>
             <td>
-            <button class="btnTable">Detaljnije / Uredi</button>
-            <button class="btnTable btnDelete">Obriši</button></td>
+            <button class="btnTable btnMoreInfo" data-UMCN="` + data[i].umcn + `">Detaljnije / Uredi</button>
+            <button class="btnTable btnDelete" data-UMCN="` + data[i].umcn + `">Obriši</button></td>
             </tr>`;
 
         table.innerHTML += row;
     }
 
     addListenersToDelete();
+    addListenersToMoreInfo();
 }
 
+function showMoreDriverInfo(e) {
+    let selectedDriver = returnDriverDataByUMCN(e.target.getAttribute("data-UMCN"));
+
+    document.getElementById("modalMoreInfoWrapperContent").innerHTML = `
+    <div id="modalMoreInfoDriverDetails">
+                    <div id="modalMoreInfoDriverPicture">
+                        <img src="./img/account_circle-black-18dp.svg">
+                        <input id="my-file-selector" type="file" name="file">
+                    </div>
+                    <div id="modalMoreInfoDriverData">
+                        <label>Ime:</label>
+                        <input type="text" value="` + selectedDriver.name + `">
+
+                        <label>Prezime:</label>
+                        <input type="text" value="` + selectedDriver.lastName + `">
+
+                        <label>Datum rodjenja:</label>
+                        <input type="text" value="` + "10.10.2010." + `">
+                        
+                        <label>JMBG:</label>
+                        <input type="text" value="` + selectedDriver.umcn + `">
+
+                        <label>Broj radne knjižice:</label>
+                        <input type="text" value="` + selectedDriver.workbookNumber + `">
+
+                        <label>Adresa:</label>
+                        <input type="text" value="` + "Kralja Petra I 20" + `">
+                    </div>
+                </div>
+                <div id="modalMoreInfoButtons">
+                <button id="btnModalInfoSaveChanges">Sačuvaj promene</button>
+                <button id="btnCancelModalInfo">Odustani</button>
+            </div>
+    `;
+
+
+    document.getElementById("btnCancelModalInfo").addEventListener("click", openCloseModalWindowInfo);
+
+    openCloseModalWindowInfo();
+}
+
+function returnDriverDataByUMCN(umcn) {
+    for (item of array) {
+        if (item.umcn === umcn) {
+            return item;
+        }
+    }
+
+}
 
 function deleteRecord(e) {
-
+    //do stuff
     openCloseModalWindowDelete();
+}
+
+function saveChanges(e) {
+    //do stuff
+    openCloseModalWindowInfo();
 }
 
 function openCloseModalWindowDelete() {
     document.getElementById("modalDelete").classList.toggle("PrikaziVisibility");
+}
+
+
+function openCloseModalWindowInfo() {
+    document.getElementById("modalMoreInfo").classList.toggle("PrikaziVisibility");
 }
