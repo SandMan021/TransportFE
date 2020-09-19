953421b6b74033083f0eb4110ea75baa14d875f0
Milos Tomasevic <milosnstomasevic@gmail.com>
2020-09-19 16:31:10 +0200

    Loading data from db for trucks nad showing modal more info and delete truck. Fixed search bar.

--- scripts/trucks.js
+++ scripts/trucks.js
@@ -1,117 +1,17 @@
-var array = [{
-    "id": 1,
-    "name": "Ivan",
-    "lastName": "Kocoljevac",
-    "umcn": "154785968459",
-    "workbookNumber": "254569"
-}, {
-    "id": 1,
-    "name": "Ivan",
-    "lastName": "Kocoljevac",
-    "umcn": "154785968459",
-    "workbookNumber": "254569"
-}, {
-    "id": 1,
-    "name": "Ivan",
-    "lastName": "Kocoljevac",
-    "umcn": "154785968459",
-    "workbookNumber": "254569"
-}, {
-    "id": 2,
-    "name": "Milos",
-    "lastName": "Tomasevic",
-    "umcn": "521236548512",
-    "workbookNumber": "856987"
-}, {
-    "id": 1,
-    "name": "Ivan",
-    "lastName": "Kocoljevac",
-    "umcn": "154785968459",
-    "workbookNumber": "254569"
-}, {
-    "id": 1,
-    "name": "Ivan",
-    "lastName": "Kocoljevac",
-    "umcn": "154785968459",
-    "workbookNumber": "254569"
-}, {
-    "id": 2,
-    "name": "Milos",
-    "lastName": "Tomasevic",
-    "umcn": "521236548512",
-    "workbookNumber": "856987"
-}, {
-    "id": 1,
-    "name": "Ivan",
-    "lastName": "Kocoljevac",
-    "umcn": "154785968459",
-    "workbookNumber": "254569"
-}, {
-    "id": 1,
-    "name": "Ivan",
-    "lastName": "Kocoljevac",
-    "umcn": "154785968459",
-    "workbookNumber": "254569"
-}, {
-    "id": 2,
-    "name": "Milos",
-    "lastName": "Tomasevic",
-    "umcn": "521236548512",
-    "workbookNumber": "856987"
-}, {
-    "id": 1,
-    "name": "Ivan",
-    "lastName": "Kocoljevac",
-    "umcn": "154785968459",
-    "workbookNumber": "254569"
-}, {
-    "id": 1,
-    "name": "Ivan",
-    "lastName": "Kocoljevac",
-    "umcn": "154785968459",
-    "workbookNumber": "254569"
-}, {
-    "id": 2,
-    "name": "Milos",
-    "lastName": "Tomasevic",
-    "umcn": "521236548512",
-    "workbookNumber": "856987"
-}, {
-    "id": 1,
-    "name": "Ivan",
-    "lastName": "Kocoljevac",
-    "umcn": "154785968459",
-    "workbookNumber": "254569"
-}, {
-    "id": 1,
-    "name": "Ivan",
-    "lastName": "Kocoljevac",
-    "umcn": "154785968459",
-    "workbookNumber": "254569"
-}, {
-    "id": 2,
-    "name": "Milos",
-    "lastName": "Tomasevic",
-    "umcn": "521236548512",
-    "workbookNumber": "856987"
-}, {
-    "id": 1,
-    "name": "Ivan",
-    "lastName": "Kocoljevac",
-    "umcn": "154785968459",
-    "workbookNumber": "254569"
-}, {
-    "id": 1,
-    "name": "Ivan",
-    "lastName": "Kocoljevac",
-    "umcn": "154785968459",
-    "workbookNumber": "254569"
-}];
-
 window.addEventListener("load", loadTruckDataFromDB);
 
 function loadTruckDataFromDB() {
-    buildTable(array);
+    axios.get("http://3.21.92.112:8080/TransportPall-0.0.1-SNAPSHOT/transportPal/trucks/getAll")
+        .then(res => {
+            nekiNizKojiJeStigaoSaAPIJA = res.data;
+            console.log("Ovo je stiglo", nekiNizKojiJeStigaoSaAPIJA);
+            array = nekiNizKojiJeStigaoSaAPIJA;
+            console.log(array);
+            buildTable(array);
+        })
+        .catch(err => {
+            console.log(err.response);
+        });
 }
 /*--------MODAL DELETE TRUCK-------*/
 document.getElementById("zatvoriModalniDetaljnije").addEventListener("click", openCloseModalWindowDelete);
@@ -127,14 +27,15 @@ function buildTable(data) {
     var table = document.getElementById('driverTableBody');
     table.innerHTML = "";
     for (var i = 0; i < data.length; i++) {
+        let date = new Date(data[i].registrationDate);
         var row = `<tr>
-                <td>` + data[i].name + `</td>
-                <td>` + data[i].umcn + `</td>
-                <td>` + data[i].umcn + `</td>
-                <td>` + data[i].workbookNumber + `</td>
+                <td>` + data[i].registrationNumber + `</td>
+                <td>` + data[i].chassisNumber + `</td>
+                <td>` + data[i].manufacturersName + `</td>
+                <td>` + date.getDay() + `.` + date.getMonth() + `.` + date.getFullYear() + `</td>
                 <td>
-                <button class="btnTable btnMoreInfo" data-VIN="` + data[i].umcn + `">Detaljnije / Uredi</button>
-                <button class="btnTable btnDelete" data-VIN="` + data[i].umcn + `">Obriši</button></td>
+                <button class="btnTable btnMoreInfo" data-VIN="` + data[i].chassisNumber + `">Detaljnije / Uredi</button>
+                <button class="btnTable btnDelete" data-VIN="` + data[i].chassisNumber + `">Obriši</button></td>
                 </tr>`;
 
         table.innerHTML += row;
@@ -150,6 +51,7 @@ function addListenersToDelete() {
         listOfDeleteButtonsInTable[i].addEventListener("click", showDeleteWindow);
     }
 }
+
 function addListenersToMoreInfo() {
     let listOfMoreInfoButtonsInTable = document.getElementsByClassName("btnMoreInfo");
     for (let i = 0; i < listOfMoreInfoButtonsInTable.length; i++) {
@@ -174,11 +76,11 @@ function showDeleteWindow(e) {
 
     document.getElementById("modalDeleteWrapperContent").innerHTML = `
     <div id="modalDeleteContentText">Da li ste sigurni da želite da obrišete kamion: </div>
-    <div id="selectedDriverDelete">Registarskih oznaka: ` + selectedTruck.umcn + `</div>
-    <div id="selectedDriverDelete">Broj šasije: ` + selectedTruck.umcn + `</div>
+    <div id="selectedDriverDelete">Registarskih oznaka: ` + selectedTruck.registrationNumber + `</div>
+    <div id="selectedDriverDelete">Broj šasije: ` + selectedTruck.chassisNumber + `</div>
     <div id="modalDeleteButtons">
-        <button id="btnDeleteDriver" class="btnModal btnModalConfirmDelete" data-UMCN="` + selectedTruck.umcn + `">Obriši</button>
-        <button id="btnCancelDeleteDriver" class="btnModal btnModalCancelDelete">Odustani od brisanja</button>
+        <button id="btnDeleteDriver" class="btnModal btnModalConfirmDelete" data-VIN="` + selectedTruck.chassisNumber + `">Obriši</button>
+        <button id="btnCancelDeleteDriver" class="btnModal btnModalCancelDelete" data-VIN="` + selectedTruck.chassisNumber + `">Odustani od brisanja</button>
     </div>
     `;
 
@@ -195,25 +97,25 @@ function showMoreInfoTrucks(e) {
     <div id="modalMoreInfoDriverDetails">
                     <div id="modalMoreInfoDriverData">
                         <label>Registarske oznake:</label>
-                        <input type="text" value="` + selectedTruck.name + `">
+                        <input type="text" value="` + selectedTruck.registrationNumber + `">
 
                         <label>Broj šasije:</label>
-                        <input type="text" value="` + selectedTruck.lastName + `">
+                        <input type="text" value="` + selectedTruck.chassisNumber + `">
 
                         <label>Boja:</label>
-                        <input type="text" value="` + "10.10.2010." + `">
+                        <input type="text" value="` + "Plava" + `">
                         
                         <label>Proizvođač:</label>
-                        <input type="text" value="` + selectedTruck.umcn + `">
+                        <input type="text" value="` + selectedTruck.manufacturersName + `">
 
                         <label>Model:</label>
-                        <input type="text" value="` + selectedTruck.workbookNumber + `">
+                        <input type="text" value="` + selectedTruck.model + `">
 
                         <label>Snaga u kW:</label>
-                        <input type="text" value="` + selectedTruck.workbookNumber + `">
+                        <input type="text" value="300">
 
                         <label>Snaga u KS:</label>
-                        <input type="text" value="` + selectedTruck.workbookNumber + `">
+                        <input type="text" value="500">
 
                         <label>Zapremina motora:</label>
                         <input type="text" value="` + "Kralja Petra I 20" + `">
@@ -238,24 +140,28 @@ function filterOutTrucksInTable(e) {
     var arrayTable = [];
     for (let item of array) {
         if (document.getElementById("searchTrucksByREG").checked) {
-            if (returnLowerText(item.name).includes(inputText)) {
+            if (returnLowerText(item.registrationNumber).includes(inputText)) {
                 arrayTable.push(item)
             }
         } else if (document.getElementById("searchTrucksByVIN").checked) {
-            if (returnLowerText(item.lastName).includes(inputText)) {
+            if (returnLowerText(item.chassisNumber).includes(inputText)) {
                 arrayTable.push(item)
             }
-        } 
+        }
     }
     buildTable(arrayTable);
 }
 
 function returnTruckDataByVIN(vin) {
     for (item of array) {
-        if (item.umcn === vin) {
+        if (item.chassisNumber === vin) {
             return item;
         }
     }
+}
+
+function returnLowerText(text) {
+    return text.toLowerCase();
 }
 
 function openCloseModalWindowInfo() {
