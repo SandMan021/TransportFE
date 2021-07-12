9175dd716d4eb71a647c55947db8f420e27fcb2d
Milos Tomasevic <milosnstomasevic@gmail.com>
2021-07-12 18:23:36 +0200

    Driver page redesigne.

--- scripts/driver.js
+++ scripts/driver.js
@@ -2,12 +2,11 @@ var array = [];
 window.addEventListener("load", getDataFromDB);
 
 function getDataFromDB() {
+    console.log('-----------------------')
     axios.get("http://3.21.92.112:8080/TransportPall-0.0.1-SNAPSHOT/transportPal/driver/getAll")
         .then(res => {
             nekiNizKojiJeStigaoSaAPIJA = res.data;
-            console.log("Ovo je stiglo", nekiNizKojiJeStigaoSaAPIJA);
             array = nekiNizKojiJeStigaoSaAPIJA;
-            console.log(array);
             buildTable(array);
         })
         .catch(err => {
@@ -29,11 +28,14 @@ function addListenersToMoreInfo() {
         listOfMoreInfoButtonsInTable[i].addEventListener("click", showMoreDriverInfo);
     }
 }
+/*-----------Hamburger menu-----------*/
+document.getElementById("closeMenu").addEventListener("click", closeHamburgerMenu);
+document.getElementById("openMenu").addEventListener("click", openHamburgerMenu);
 /*-----------Modal delete------------ */
 document.getElementById("zatvoriModalniDetaljnije").addEventListener("click", openCloseModalWindowDelete);
 document.getElementById("modalDelete").addEventListener("click", checkIfClickedModalDelete);
 
-/*-----------Modal more info---------------*/
+/*-----------Modal edit---------------*/
 document.getElementById("closeModalMoreInfo").addEventListener("click", openCloseModalWindowInfo);
 document.getElementById("modalMoreInfo").addEventListener("click", checkIfClickedModalMoreInfo);
 
@@ -65,6 +67,18 @@ function checkIfClickedModalAddDriver(e) {
     }
 }
 
+function closeHamburgerMenu() {
+    document.getElementById("sideBar").style.marginRight = '-120px';
+    document.getElementById("closeMenu").style.display = 'none';
+    document.getElementById("openMenu").style.display = 'block';
+}
+
+function openHamburgerMenu() {
+    document.getElementById("sideBar").style.marginRight = '0';
+    document.getElementById("closeMenu").style.display = 'block';
+    document.getElementById("openMenu").style.display = 'none';
+}
+
 function buildTable(data) {
     var table = document.getElementById('driverTableBody');
     table.innerHTML = "";
@@ -73,12 +87,12 @@ function buildTable(data) {
             <td>` + data[i].name + `</td>
             <td>` + data[i].lastName + `</td>
             <td>` + data[i].umcn + `</td>
-            <td>` + data[i].workbookNumber + `</td>
-            <td>` + data[i].workbookNumber + `</td>
-            <td>` + data[i].workbookNumber + `</td>
+            <td>` + data[i].numberOfPassport + `</td>
+            <td>` + data[i].numberOfId + `</td>
+            <td>` + data[i].workContract + `</td>
             <td>
-            <button class="btnTable btnMoreInfo" data-UMCN="` + data[i].umcn + `">Detaljnije / Uredi</button>
-            <button class="btnTable btnDelete" data-UMCN="` + data[i].umcn + `">Obriši</button></td>
+            <span class="btnTable btnMoreInfo" data-UMCN="` + data[i].umcn + `">Uredi</span>
+            <span class="btnTable btnDelete" data-UMCN="` + data[i].umcn + `">Obriši</span></td>
             </tr>`;
 
         table.innerHTML += row;
@@ -108,7 +122,7 @@ function filterOutDriversInTable(e) {
                 arrayTable.push(item)
             }
         } else if (document.getElementById("searchDriverByWN").checked) {
-            if (returnLowerText(item.workbookNumber).includes(inputText)) {
+            if (returnLowerText(item.workContract).includes(inputText)) {
                 arrayTable.push(item)
             }
         }
@@ -124,23 +138,70 @@ function createNewDriver(e) {
     let name = document.getElementById("newDriverName").value;
     let lastName = document.getElementById("newDriverLastName").value;
     let umcn = document.getElementById("newDriverUMCN").value;
-    let workbookNumber = document.getElementById("newDriverwoorkNumber").value;
-    let DOB = document.getElementById("newDriverDOB").value;
-    let addres = document.getElementById("newDriverAddres").value;
+    let numberOfPassport = document.getElementById("newDriverPassportNumber").value;
+    let numberOfId = document.getElementById("newDriverIdNumber").value;
+    let workContract = document.getElementById("newDriverWorkNumber").value;
+
     let newDriver = {
         "name": name,
         "lastName": lastName,
         "umcn": umcn,
-        "workbookNumber": workbookNumber
+        "numberOfPassport": numberOfPassport,
+        "workContract": workContract,
+        "numberOfId": numberOfId
     }
-    axios.post("http://3.21.92.112:8080/TransportPall-0.0.1-SNAPSHOT/transportPal/driver/insertNew", newDriver)
-        .then(res => {
-            if (res.status === 200) {
-                getDataFromDB();
-                showSuccessScreenOnModalNewDriver();
-            }
-        })
-        .catch(err => console.log(err));
+    if (validateFieldsDriver(newDriver)) {
+
+        axios.post("http://3.21.92.112:8080/TransportPall-0.0.1-SNAPSHOT/transportPal/driver/insertNew", newDriver)
+            .then(res => {
+                if (res.status === 200) {
+                    getDataFromDB();
+                    showSuccessScreenOnModalNewDriver();
+                }
+            })
+            .catch(err => 
+                err?
+                alert('Greška, neki podaci već postoje u bazi i ne mogu biti vezani za drugog vozača (JMBG, broj lk. ...)'):null);
+    }
+}
+
+function validateFieldsDriver(newDriver) {
+    var isError = false;
+    var errorText = 'Greška prilikom unosa podatak:';
+    if (newDriver.name == '') {
+        isError = true;
+        errorText = errorText + '\n-Niste uneli ime.';
+    }
+    if (newDriver.lastName == '') {
+        isError = true;
+        errorText = errorText + '\n-Niste uneli prezime.';
+    }
+    if (newDriver.umcn == '') {
+        isError = true;
+        errorText = errorText + '\n-Niste uneli JMBG.';
+    } else if (newDriver.umcn.length != 13) {
+        isError = true;
+        errorText = errorText + '\n-JMBG mora imati 13 cifara.';
+    }
+    if (newDriver.numberOfPassport == '') {
+        isError = true;
+        errorText = errorText + '\n-Niste uneli broj pasoša.';
+    }
+    if (newDriver.numberOfId == '') {
+        isError = true;
+        errorText = errorText + '\n-Niste uneli broj lične karte.';
+    }
+    if (newDriver.workContract == '') {
+        isError = true;
+        errorText = errorText + '\n-Niste uneli broj ugovora o radu.';
+    } 
+    if(isError){
+        alert(errorText);
+    }
+    else {
+        return true;
+    }
+    
 }
 
 function showSuccessScreenOnModalNewDriver() {
@@ -158,38 +219,43 @@ function showSuccessScreenOnModalNewDriver() {
     </div>
     `;
 
-    document.getElementById("closeModalSuccessAddDriver").addEventListener("click", closeModalSuccessAddDriverReturnBackOldHTML);
+    document.getElementById("closeModalSuccessAddDriver").addEventListener("click", openCloseModalAddDriver);
     document.getElementById("modalWrapperAdd").style.width = "50%";
 }
 
-function closeModalSuccessAddDriverReturnBackOldHTML() {
-    openCloseModalAddDriver();
+var loadFile = function (event) {
+    var image = document.getElementById('output');
+    image.src = URL.createObjectURL(event.target.files[0]);
+};
+
+function fillmodalAddDriverWithHTML() {
     let wrapperContetn = document.getElementById("modalAddDriverWrapperContent");
     wrapperContetn.innerHTML = ` 
         <div id="modalAddDriverDetails">
         <div id="modalMoreInfoDriverPicture">
-            <div><img src="./img/account_circle-black-18dp.svg"></div>
-            <input id="my-file-selector" type="file" name="file">
+        <div ><img src="./img/account_circle-black-18dp.svg" id="output"></div>
+        <input id="file" type="file" name="file" accept="image/*" class="custom-file-input" onchange="loadFile(event)">
         </div>
         <div id="modalMoreInfoDriverData">
-            <label>Ime:</label>
-            <input type="text" id="newDriverName">
+                        <label>Ime:</label>
+                        <input type="text" id="newDriverName">
 
-            <label>Prezime:</label>
-            <input type="text" id="newDriverLastName">
+                        <label>Prezime:</label>
+                        <input type="text" id="newDriverLastName">
 
-            <label>Datum rodjenja:</label>
-            <input type="text" id="newDriverDOB">
+                        <label>JMBG:</label>
+                        <input type="number" maxlength="13" id="newDriverUMCN">
 
-            <label>JMBG:</label>
-            <input type="text" id="newDriverUMCN">
+                        <label>Broj pasoša:</label>
+                        <input type="number" id="newDriverPassportNumber">
 
-            <label>Broj radne knjižice:</label>
-            <input type="text" id="newDriverwoorkNumber">
+                        <label>Broj lične karte:</label>
+                        <input type="number" id="newDriverIdNumber">
 
-            <label>Adresa:</label>
-            <input type="text" id="newDriverAddres">
-        </div>
+                        <label>Broj ugovora o radu:</label>
+                        <input type="number" id="newDriverWorkNumber">
+
+                    </div>
     </div>
     <div id="modalAddDriverButtons">
         <button id="btnModalAddNewDriverToDB" class="btnModal btnModalInfoSaveChanges">Dodaj vozača</button>
@@ -208,7 +274,7 @@ function showDeleteWindow(e) {
     <div id="selectedDriverDelete">` + selectedDriver.name + ` ` + selectedDriver.lastName + `</div>
     <div id="modalDeleteButtons">
         <button id="btnDeleteDriver" class="btnModal btnModalConfirmDelete" data-UMCN="` + selectedDriver.umcn + `">Obriši</button>
-        <button id="btnCancelDeleteDriver" class="btnModal btnModalCancelDelete">Odustani od brisanja</button>
+        <button id="btnCancelDeleteDriver" class="btnModal btnModalCancelDelete">Odustani</button>
     </div>
     `;
 
@@ -218,33 +284,52 @@ function showDeleteWindow(e) {
     openCloseModalWindowDelete();
 }
 
+function showSuccessScreenOnModalDriverMoreInfo() {
+    let wrapperContetn = document.getElementById("modalMoreInfoWrapperContent");
+    wrapperContetn.innerHTML = `
+    <div>
+        <svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 130.2 130.2" id="checkAnimation">
+        <circle class="path circle" fill="none" stroke="#73AF55" stroke-width="6" stroke-miterlimit="10" cx="65.1" cy="65.1" r="62.1"/>
+        <polyline class="path check" fill="none" stroke="#73AF55" stroke-width="6" stroke-linecap="round" stroke-miterlimit="10" points="100.2,40.2 51.5,88.8 29.8,67.5 "/>
+        </svg>
+    </div>
+    <div id="modalSuccess">Izmene uspešno upisane u bazu podataka!</div>
+    <div>
+    <button class="btnModal btnModalInfoSaveChanges" id="closeModalSuccessAddDriver">Zatvori</button>
+    </div>
+    `;
+
+    document.getElementById("closeModalSuccessAddDriver").addEventListener("click", openCloseModalWindowInfo);
+    document.getElementById("modalWrapperInfo").style.width = "50%";
+}
+
 function showMoreDriverInfo(e) {
     let selectedDriver = returnDriverDataByUMCN(e.target.getAttribute("data-UMCN"));
 
     document.getElementById("modalMoreInfoWrapperContent").innerHTML = `
     <div id="modalMoreInfoDriverDetails">
                     <div id="modalMoreInfoDriverPicture">
-                        <div><img src="./img/account_circle-black-18dp.svg"></div>
-                        <input id="my-file-selector" type="file" name="file">
+                    <div ><img src="./img/account_circle-black-18dp.svg" id="output"></div>
+                    <input id="file" type="file" name="file" accept="image/*" class="custom-file-input" onchange="loadFile(event)">
                     </div>
                     <div id="modalMoreInfoDriverData">
                         <label>Ime:</label>
-                        <input type="text" value="` + selectedDriver.name + `">
+                        <input type="text" id="driverName" value="` + selectedDriver.name + `">
 
                         <label>Prezime:</label>
-                        <input type="text" value="` + selectedDriver.lastName + `">
-
-                        <label>Datum rodjenja:</label>
-                        <input type="text" value="` + "10.10.2010." + `">
+                        <input type="text" id="driverLastName" value="` + selectedDriver.lastName + `">
                         
                         <label>JMBG:</label>
-                        <input type="text" value="` + selectedDriver.umcn + `">
+                        <input type="number" id="driverUMCN" maxlength="13" value="` + selectedDriver.umcn + `">
 
-                        <label>Broj radne knjižice:</label>
-                        <input type="text" value="` + selectedDriver.workbookNumber + `">
+                        <label>Broj pasoša:</label>
+                        <input type="number" id="driverPassportNumber" value="` + selectedDriver.numberOfPassport + `">
 
-                        <label>Adresa:</label>
-                        <input type="text" value="` + "Kralja Petra I 20" + `">
+                        <label>Broj lične karte:</label>
+                        <input type="number" id="driverIdNumber" value="` + selectedDriver.numberOfId + `">
+
+                        <label>Broj ugovora o radu:</label>
+                        <input type="number" id="driverWorkNumber" value="` + selectedDriver.workContract + `">
                     </div>
                 </div>
                 <div id="modalMoreInfoButtons">
@@ -253,6 +338,7 @@ function showMoreDriverInfo(e) {
             </div>
     `;
 
+    document.getElementById("btnModalInfoSaveChanges").addEventListener("click", saveChanges);
     document.getElementById("btnCancelModalInfo").addEventListener("click", openCloseModalWindowInfo);
 
     openCloseModalWindowInfo();
@@ -299,9 +385,41 @@ function showSuccessScreenOnModalDeleteDriver() {
     document.getElementById("modalWrapper").style.width = "50%";
 }
 
-function saveChanges(e) {
-    //do stuff
-    openCloseModalWindowInfo();
+function saveChanges() {
+    let name = document.getElementById("driverName").value;
+    let lastName = document.getElementById("driverLastName").value;
+    let umcn = document.getElementById("driverUMCN").value;
+    let numberOfPassport = document.getElementById("driverPassportNumber").value;
+    let numberOfId = document.getElementById("driverIdNumber").value;
+    let workContract = document.getElementById("driverWorkNumber").value;
+
+    let driver = {
+        "name": name,
+        "lastName": lastName,
+        "umcn": umcn,
+        "numberOfPassport": numberOfPassport,
+        "workContract": workContract,
+        "numberOfId": numberOfId
+    }
+    if (validateFieldsDriver(driver)) {
+
+        // axios.post("http://3.21.92.112:8080/TransportPall-0.0.1-SNAPSHOT/transportPal/driver/insertNew", driver)
+        //     .then(res => {
+        //         if (res.status === 200) {
+        //             getDataFromDB();
+        //             showSuccessScreenOnModalDriverMoreInfo();
+        //         }
+        //     })
+            // .catch(err => 
+            //     err?
+            //     alert('Greška prilikom komunikacije sa bazom podataka.'):null);
+    }
+    console.log('ovde');
+    getDataFromDB();
+    console.log('ovde2');
+    showSuccessScreenOnModalDriverMoreInfo();
+
+    //openCloseModalWindowInfo();
 }
 
 function openCloseModalWindowDelete() {
@@ -310,8 +428,11 @@ function openCloseModalWindowDelete() {
 
 function openCloseModalWindowInfo() {
     document.getElementById("modalMoreInfo").classList.toggle("PrikaziVisibility");
+    document.getElementById("modalWrapperInfo").style.width = "70%";
 }
 
 function openCloseModalAddDriver() {
+    fillmodalAddDriverWithHTML();
     document.getElementById("modalAddDriver").classList.toggle("PrikaziVisibility");
+
 }
